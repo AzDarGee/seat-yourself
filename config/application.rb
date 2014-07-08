cf5f5d57ef4b16e386e08fb6ba827e675e9f357f
Ash Darji <ashishdarji88@gmail.com>
2014-07-07 22:27:10 -0400

    added better fonts

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module SeatYourself
 
     config.assets.initialize_on_precompile = false
 
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   end
 end
