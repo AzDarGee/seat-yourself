28ef706f3c11013ae60507884871a594e88e28ce
Ash Darji <ashishdarji88@gmail.com>
2014-07-08 22:34:18 -0400

    cant remember, forgot to commit

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,15 @@
 class Restaurant < ActiveRecord::Base
-  validates :name, presence: true
 
+  # ASSOCIATIONS
   belongs_to :owner
   belongs_to :category
   has_many :reservations
   has_many :reviews
+  has_many :customers, :through => :reservations
+
+  # VALIDATIONS
+  validates :name, presence: true
+
 
 
   # Make new columns in schema for opening/closing hours of the restaurants
